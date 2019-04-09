665c2675903c17af6f50136286e5094d9182a716
Cristian <cristian.rock78@gmail.com>
2019-04-09 16:51:09 -0400

    Configurado vue.js con laravel

---
+++ resources/views/content/content.blade.php
@@ -0,0 +1,43 @@
+   
+   @extends('main')
+   @section('content')    
+        <template v-if="menu==0">
+                <Example-Component></Example-Component>
+        </template>
+        <template v-if="menu==1">
+                <h1>"contenido menu 1"</h1>
+        </template>
+        <template v-if="menu==2">
+                <h1>"contenido menu 2"</h1>
+        </template>
+        <template v-if="menu==3">
+                <h1>"contenido menu 3"</h1>
+        </template>
+        <template v-if="menu==4">
+                <h1>"contenido menu 4"</h1>
+        </template>
+        <template v-if="menu==5">
+                <h1>"contenido menu 5"</h1>
+        </template>
+        <template v-if="menu==6">
+                <h1>"contenido menu 6"</h1>
+        </template>
+        <template v-if="menu==7">
+                <h1>"contenido menu 7"</h1>
+        </template>
+        <template v-if="menu==8">
+                <h1>"contenido menu 8"</h1>
+        </template>
+        <template v-if="menu==9">
+                <h1>"contenido menu 9"</h1>
+        </template>
+        <template v-if="menu==10">
+                <h1>"contenido menu 10"</h1>
+        </template>
+        <template v-if="menu==11">
+                <h1>"contenido menu 11"</h1>
+        </template>
+        <template v-if="menu==12">
+                <h1>"contenido menu 12"</h1>
+        </template>
+   @endsection
