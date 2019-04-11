0f737d5490064a578aa9eeb0c90fada73e9f0a8c
Cristian <cristian.rock78@gmail.com>
2019-04-11 13:10:26 -0400

    Metodo POST crear registro de categoria y ventana modal

--- resources/js/components/Categorie.vue
+++ resources/js/components/Categorie.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
     <main class="main">
          <!-- Breadcrumb -->
          <ol class="breadcrumb">
@@ -11,7 +11,9 @@
              <div class="card">
                  <div class="card-header">
                      <i class="fa fa-align-justify"></i> Categorías
-                     <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalNuevo">
+                     <button type="button" 
+                             class="btn btn-secondary" 
+                             @click="abrirModal('categoria','registrar')">
                          <i class="icon-plus"></i>&nbsp;Nuevo
                      </button>
                  </div>
@@ -40,7 +42,9 @@
                          <tbody>
                              <tr v-for = "categoria in arrayCategorie" :key = "categoria.id">
                                  <td>
-                                     <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modalNuevo">
+                                     <button type="button" 
+                                             class="btn btn-warning btn-sm"
+                                             @click="abrirModal('categoria','registrar',categoria)">
                                        <i class="icon-pencil"></i>
                                      </button> &nbsp;
                                      <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modalEliminar">
@@ -90,12 +94,12 @@
              <!-- Fin ejemplo de tabla Listado -->
          </div>
          <!--Inicio del modal agregar/actualizar-->
-         <div class="modal fade" id="modalNuevo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+         <div :class="{'mostrar': modal}" class="modal fade"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
              <div class="modal-dialog modal-primary modal-lg" role="document">
                  <div class="modal-content">
                      <div class="modal-header">
-                         <h4 class="modal-title">Agregar categoría</h4>
-                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                         <h4 class="modal-title" v-text="tituloModal"></h4>
+                         <button type="button" class="close" @click="cerrarModal()" aria-label="Close">
                            <span aria-hidden="true">×</span>
                          </button>
                      </div>
@@ -104,21 +108,23 @@
                              <div class="form-group row">
                                  <label class="col-md-3 form-control-label" for="text-input">Nombre</label>
                                  <div class="col-md-9">
-                                     <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre de categoría">
+                                     <input type="text" v-model="nombre"  class="form-control" placeholder="Nombre de categoría">
                                      <span class="help-block">(*) Ingrese el nombre de la categoría</span>
                                  </div>
                              </div>
                              <div class="form-group row">
                                  <label class="col-md-3 form-control-label" for="email-input">Descripción</label>
                                  <div class="col-md-9">
-                                     <input type="email" id="descripcion" name="descripcion" class="form-control" placeholder="Enter Email">
+                                     <input type="email" v-model="descripcion" class="form-control" placeholder="Descripcion">
                                  </div>
                              </div>
                          </form>
                      </div>
                      <div class="modal-footer">
-                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                         <button type="button" class="btn btn-primary">Guardar</button>
+                         <button type="button" class="btn btn-secondary" @click="cerrarModal()">Cerrar</button>
+                         <button type="button" v-if="tipoAccion==1" class="btn btn-primary" @click="registrarCategoria()">Guardar</button>
+                         <button type="button" v-if="tipoAccion==2" class="btn btn-primary">Actualizar</button>
+
                      </div>
                  </div>
                  <!-- /.modal-content -->
@@ -153,34 +159,106 @@
 </template>
 
 <script>
-    export default {
-     data()
-     {
+
+export default {
+    
+    data(){
       return {
        nombre: '',
        descripcion: '',
-       arrayCategorie: []
+       arrayCategorie: [],
+       modal: 0,
+       tituloModal: '',
+       tipoAccion: 0
       }   
-     },
-     methods: {
-      listCategoria()
-      {
-       let me = this;
-        axios.get('/categoria')
-        .then(function (response) {
-          me.arrayCategorie = response.data;
-        })
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
-        .then(function () {
-          // always executed
-        });
-      }
-     },
-        mounted() {
+    },
+
+    //Funciones    
+    methods: {
+        listCategoria(){
+            let me = this;
+                axios.get('/categoria')
+                            .then(function (response) {
+                            me.arrayCategorie = response.data;
+                            console.log(response.data);
+                            })
+                            .catch(function (error) {
+                            // handle error
+                            console.log(error);
+                            })
+                            .then(function () {
+                            // always executed
+                });
+        },
+        
+        registrarCategoria(){
+            let me = this;
+            
+            axios.post('/categoria/registrar', 
+                       {
+                           'nombre': this.nombre,
+                           'descripcion': this.descripcion
+                       }).then( function (response) {
+                           //Si todo sale bien se ejecuta cerrar y listar categoria  
+                           me.cerrarModal();
+                           me.listCategoria();
+                       }).catch(function (error) {
+                                    console.log(error);
+            });
+        },
+
+        abrirModal(modelo,accion,data=[]){
+            switch (modelo) 
+            {
+                case 'categoria':
+                {
+                    switch (accion) 
+                    {
+                        case 'registrar':
+                            this.modal = 1;
+                            this.tituloModal = 'Registrar';
+                            this.nombre = '';
+                            this.descripcion = '';
+                            this.tipoAccion = 1;
+                        
+                            break;
+                        
+                        case 'actualizar':
+                            break;
+                    }
+                }
+            }
+        },
+        cerrarModal()
+        {
+            this.modal=0;
+            this.tituloModal='';
+            this.nombre='';
+            this.descripcion='';
+        },
+    },
+
+    //Funcion pre-determinada con la data cargada
+    mounted() {
            this.listCategoria();
-        }   
+    }
+    
+    
     }
 </script>
+
+
+<style>
+    .modal-content{
+        width: 100% !important;
+        position: absolute;
+    }
+    .mostrar{
+        display: list-item !important;
+        opacity: 1 !important;
+        position: absolute !important;
+        background-color: #3c29297a !important; 
+
+    }
+</style>
+
